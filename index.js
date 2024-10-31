20ec2f1b5083f3d340415e4cae58295a3bc73f15
ali arman <fiver89067@gmail.com>
2024-10-31 19:31:07 +0500

    Updates

--- index.js
+++ index.js
@@ -9,15 +9,22 @@ import SubUnit from "./model/SubUnits.js";
 import UnitModel from "./model/Unit.js";
 import UserModel from "./model/User.js";
 import bcrypt from "bcryptjs";
+import Stripe from "stripe";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     origin: process.env.CLIENT_ORIGIN,
     credentials: true,
   })
 );
+
+const STRIPE = new Stripe(process.env.STRIPE_API_KEY);
+const FRONTEND_URL = process.env.CLIENT_ORIGIN;
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -75,6 +82,9 @@ app.post("/create-user", async (req, res) => {
       }
       await newUser.save();
       const { password: newPassword, ...rest } = newUser.toObject();
+      const token = jwt.sign({ rest }, process.env.JWT_SECRET);
+      res.cookie("highschoolprep", token);
+
       res.status(201).json({ success: true, data: rest });
     }
   } catch (error) {
@@ -101,6 +111,8 @@ app.post("/get-user", async (req, res) => {
       });
     }
     const { password: modelPass, ...rest } = findUser.toObject();
+    const token = jwt.sign({ rest }, process.env.JWT_SECRET);
+    res.cookie("highschoolprep", token);
     res.status(200).json({ success: true, data: rest });
   } catch (error) {
     console.log(error);
@@ -117,6 +129,8 @@ app.post("/create-user-google", async (req, res) => {
       const checkPass = await bcrypt.compare(uid, findUser.password);
       if (checkPass) {
         const { password: modelPass, ...rest } = findUser.toObject();
+        const token = jwt.sign({ rest }, process.env.JWT_SECRET);
+        res.cookie("highschoolprep", token);
         return res.status(200).json({ success: true, data: rest });
       } else {
         return res
@@ -132,6 +146,8 @@ app.post("/create-user-google", async (req, res) => {
     newUser.image = image;
     await newUser.save();
     const { password: newUserPass, ...rest } = newUser.toObject();
+    const token = jwt.sign({ rest }, process.env.JWT_SECRET);
+    res.cookie("highschoolprep", token);
     res.status(201).json({ success: true, data: rest });
   } catch (error) {
     res.status(400).json({ success: false, message: "Something went wrong" });
@@ -139,6 +155,127 @@ app.post("/create-user-google", async (req, res) => {
   }
 });
 
-app.get("/test", (req, res) => {
-  res.send("Hello World");
+let PACKAGESNAMES = {
+  oneMonth: "1 Month",
+  fourMonth: "4 Months",
+  oneYear: "1 Year",
+};
+
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    const { packageName } = req.body;
+    console.log(packageName);
+
+    if (
+      packageName === PACKAGESNAMES.oneMonth ||
+      packageName === PACKAGESNAMES.fourMonth ||
+      packageName === PACKAGESNAMES.oneYear
+    ) {
+      const price = getPriceByName(packageName);
+      const description = getDescByName(packageName);
+
+      const sessionData = await STRIPE.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: "usd",
+              unit_amount: price * 100,
+              product_data: {
+                name: packageName + " Package",
+                description: description,
+              },
+            },
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        metadata: {
+          packageName: packageName,
+        },
+        success_url: `${FRONTEND_URL}/payment-completed?packageName=${packageName}`,
+        cancel_url: `${FRONTEND_URL}/payment-cancelled`,
+      });
+      if (!sessionData.url) {
+        return res
+          .status(400)
+          .json({ success: false, message: "Url is not provided by stripe" });
+      }
+      res.status(201).json({ url: sessionData.url });
+    } else {
+      return res
+        .status(400)
+        .json({ success: false, message: "Wrong packages name" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.raw.message });
+  }
+});
+
+const getPriceByName = (packageName) => {
+  const price =
+    packageName === PACKAGESNAMES.oneMonth
+      ? 5
+      : packageName === PACKAGESNAMES.fourMonth
+      ? 10
+      : packageName === PACKAGESNAMES.oneYear
+      ? 15
+      : 0;
+  return price;
+};
+
+const getDescByName = (packageName) => {
+  let description;
+  if (packageName === PACKAGESNAMES.oneMonth) {
+    description =
+      "The package will be available for one month, giving you plenty of time to explore its contents and enjoy its benefits before it's gone.";
+  }
+  if (packageName === PACKAGESNAMES.fourMonth) {
+    description =
+      "The package will be available for four months, allowing you to fully enjoy and explore its offerings at your own pace.";
+  }
+  if (packageName === PACKAGESNAMES.oneYear) {
+    description =
+      "This package lasts for an entire year, providing you with a wealth of benefits and experiences to enjoy throughout the months.";
+  }
+  return description;
+};
+
+app.post("/check-user", async (req, res) => {
+  try {
+    const { userId } = req.body;
+    if (!userId) {
+      return res.status(200).json({
+        success: false,
+        message: "User id not provided in request body",
+      });
+    }
+    const cookie = req.cookies.highschoolprep;
+    if (!cookie) {
+      return res.status(200).json({
+        success: false,
+        message: "Cookie not found",
+      });
+    }
+    const {
+      rest: { _id },
+    } = jwt.verify(cookie, process.env.JWT_SECRET);
+    if (!_id) {
+      return res.status(200).json({
+        success: false,
+        message: "_id not found from cookie",
+      });
+    }
+    if (_id !== userId) {
+      return res
+        .status(200)
+        .json({ success: false, message: "Id's are not matched" });
+    } else {
+      return res.status(200).json({ success: true, message: "User is valid" });
+    }
+  } catch (error) {
+    console.log(error);
+
+    res.status(400).json({ success: false, message: "Somethin went wrong" });
+  }
 });
