8cf4d6e4894885fedcf4df0c326a9dfc22c95e66
Ali Arman <ali@gmail.com>
2024-11-07 12:59:43 -0800

    Add timeStamp to User Model

--- model/User.js
+++ model/User.js
@@ -9,23 +9,26 @@ const UserUnitSchema = new mongoose.Schema({
   playedTime: { type: String },
 });
 
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  image: {
-    type: String,
-    default:
-      "https://static.vecteezy.com/system/resources/thumbnails/004/511/281/small/default-avatar-photo-placeholder-profile-picture-vector.jpg",
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    image: {
+      type: String,
+      default:
+        "https://static.vecteezy.com/system/resources/thumbnails/004/511/281/small/default-avatar-photo-placeholder-profile-picture-vector.jpg",
+    },
+    isPremium: { type: Boolean, default: false },
+    packageName: { type: String },
+    purchaseAt: { type: Date },
+    expiresAt: { type: Date },
+    packagePrice: { type: Number },
+    oAuth: { type: Boolean },
+    playedSubUnits: [UserUnitSchema],
   },
-  isPremium: { type: Boolean, default: false },
-  packageName: { type: String },
-  purchaseAt: { type: Date },
-  expiresAt: { type: Date },
-  packagePrice: { type: Number },
-  oAuth: { type: Boolean },
-  playedSubUnits: [UserUnitSchema],
-});
+  { timestamps: true }
+);
 
 const UserModel = mongoose.model("user", UserSchema);
 
